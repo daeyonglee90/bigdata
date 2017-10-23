d96f0c26e34301ee85027a09f96d9d387e2582f2
dleo2 <dleo2@DESKTOP-DLDRUO4>
2017-10-23 18:12:56 +0900

    1. 회원가입 Validation 처리 및 JPA 회원테이블 작성

--- src/main/java/com/pjt/controller/LoginController.java
+++ src/main/java/com/pjt/controller/LoginController.java
@@ -1,16 +1,32 @@
 package com.pjt.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pjt.vo.MemberVO;
 
 @RequestMapping("/login")
 @Controller
 public class LoginController {
 	
 	@GetMapping("/join")
-	public String joinForm()
+	public String joinGET()
 	{
 		return "thymeleaf/login/join";
+	}
+	
+	@PostMapping("/join")
+	public String joinPOST(@ModelAttribute("memberVO") MemberVO memberVO, Model model)
+	{
+		System.out.println("/login/join");
+		
+		
+		
+		
+		return "redirect:/index";
 	}
 }
